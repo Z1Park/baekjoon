de50e37b4126967b0464c34c046ce34e7bdc052d
Z1Park <82518170+Z1Park@users.noreply.github.com>
2022-09-26 16:49:51 +0900

    [Gold V] Title: 스타트링크, Time: 224 ms, Memory: 129724 KB -BaekjoonHub

--- 백준/Gold/5014. 스타트링크/스타트링크.py
+++ 백준/Gold/5014. 스타트링크/스타트링크.py
@@ -1,21 +1,18 @@
 from collections import deque
 
 f, s, g, u, d = map(int, input().split())
-floor = [0] * (f+1)
-visit = [0] * (f+1)
-flag = True
+floor = [-1 for _ in range(f+1)]
 def bfs(tmp):
     que = deque()
-    visit[tmp] = 1
     que.append(tmp)
+    floor[tmp] = 0
     while que:
         x = que.popleft()
         if x == g:
             print(floor[x])
             return
         for i in (x+u, x-d):
-            if 1 <= i <= f and not visit[i]:
-                visit[i] = 1
+            if 1 <= i <= f and floor[i] == -1:
                 floor[i] = floor[x] + 1
                 que.append(i)
     print("use the stairs")
