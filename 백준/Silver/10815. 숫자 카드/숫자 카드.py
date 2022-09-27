9cb944fd10dab32739edc0c777474529721287e1
Z1Park <82518170+Z1Park@users.noreply.github.com>
2022-09-27 15:11:36 +0900

    [Silver V] Title: 숫자 카드, Time: 576 ms, Memory: 255156 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,21 +1,10 @@
 n = int(input())
-lst = list(map(int, input().split()))
-lst.sort()
+lst = set(map(int, input().split()))
 m = int(input())
 check = list(map(int, input().split()))
 result = []
 for c in check:
-    front, end = 0, n - 1
-    while front + 1 != end:
-        mid = (front + end) // 2
-        if lst[mid] < c:
-            front = mid
-        elif lst[mid] > c:
-            end = mid
-        else:
-            front = mid
-            break
-    if lst[front] == c or lst[end] == c:
+    if c in lst:
         result.append(1)
     else:
         result.append(0)
