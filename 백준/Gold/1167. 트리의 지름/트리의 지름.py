94fedf2345cd09d4ed9a42649ba593a3bcca1952
Z1Park <82518170+Z1Park@users.noreply.github.com>
2022-09-30 14:17:26 +0900

    [Gold II] Title: 트리의 지름, Time: 600 ms, Memory: 286796 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -0,0 +1,21 @@
+import sys
+sys.setrecursionlimit(100000)
+def dfs(node, weight):
+    distance[node] = weight
+    for i in tree[node]:
+        if distance[i[0]] == -1:
+            dfs(i[0], weight+i[1])
+
+n = int(input())
+tree = [[] for _ in range(n+1)]
+for i in range(n):
+    tmp = list(map(int, input().split()))[:-1]
+    for j in range(1, len(tmp), 2):
+        tree[tmp[0]].append([tmp[j], tmp[j+1]])
+
+distance = [-1 for _ in range(n+1)]
+dfs(1, 0)
+last = distance.index(max(distance))
+distance = [-1 for _ in range(n+1)]
+dfs(last, 0)
+print(max(distance))
