cc78eef9218861714b0a8ae2eaa65668e59a4cd8
Z1Park <82518170+Z1Park@users.noreply.github.com>
2022-10-23 19:17:40 +0900

    [Gold III] Title: LCS 3, Time: 200 ms, Memory: 124160 KB -BaekjoonHub

---
+++ 백준/Gold/1958. LCS 3/LCS 3.py
@@ -0,0 +1,12 @@
+a = [' '] + list(input().rstrip())
+b = [' '] + list(input().rstrip())
+c = [' '] + list(input().rstrip())
+dp = [[[0 for _ in range(len(c))] for _ in range(len(b))] for _ in range(len(a))]
+for i in range(1, len(a)):
+    for j in range(1, len(b)):
+        for k in range(1, len(c)):
+            if a[i] == b[j] and b[j] == c[k]:
+                dp[i][j][k] = dp[i-1][j-1][k-1] + 1
+            else:
+                dp[i][j][k] = max(dp[i-1][j][k], dp[i][j-1][k], dp[i][j][k-1])
+print(dp[-1][-1][-1])
