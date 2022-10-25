7ba9aa0e8589576e464c4cd50bbe8671e068b308
Z1Park <82518170+Z1Park@users.noreply.github.com>
2022-10-25 13:07:25 +0900

    [Gold IV] Title: 공유기 설치, Time: 252 ms, Memory: 117128 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,17 +1,22 @@
 n, m = map(int, input().split())
 houses = [int(input()) for _ in range(n)]
 houses.sort()
-start, end = 1, houses[n-1] - houses[0]
+
+def check(dist):
+    count = 1
+    curr = houses[0]
+    for i in range(1, n):
+        if houses[i] >= curr + dist:
+            count += 1
+            curr = houses[i]
+    return count
+
+start = 1
+end = max(houses)
 while start <= end:
     mid = (start + end) // 2
-    tmp = houses[0]
-    count = 1
-    for i in range(1, n):
-        if houses[i] - tmp >= mid:
-            tmp = houses[i]
-            count += 1
-    if count >= m:
-        start = mid + 1
-    else:
+    if check(mid) < m:
         end = mid - 1
+    else:
+        start = mid + 1
 print(end)
