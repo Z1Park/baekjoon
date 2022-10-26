7bb650dc99d8fdca1176d47d2b95ffedae780b31
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2022-10-26 11:02:16 +0900

    [Gold IV] Title: 문자열 폭발, Time: 300 ms, Memory: 211536 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,20 +1,10 @@
 import sys
-input = sys.stdin.readline
-
-T = input()
-T = T[:-1]
-A = list(input())
-A = A[:-1]
-la = len(A)
-lt = len(T)
-left = []
-idx = 0
-while idx < lt:
-    left.append(T[idx])
-    if left[-la:] == A:
-        left[-la:] = []
-    idx += 1
-if left:
-    print(''.join(left))
-else:
-    print("FRULA")
+input = lambda : sys.stdin.readline().rstrip()
+txt = list(input())
+bomb = list(input())
+res = []
+for t in txt:
+    res.append(t)
+    if len(res) >= len(bomb) and res[-len(bomb):] == bomb:
+        res[-len(bomb):] = []
+print(''.join(res) if res else "FRULA")
