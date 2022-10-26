086f94ab72a1f3c8dfebc78e54de14adbbabcc5b
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2022-10-26 09:12:13 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 121880 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,19 +1,9 @@
-import sys
-input = sys.stdin.readline
-
+from collections import deque
 n = int(input())
-s = []
-for i in range(1, n + 1):
-    s.append(i)
-while (len(s) > 1):
-    tmp = []
-    for i in range(n):
-        if i%2 == 1:
-            tmp.append(s[i])
-    s.clear()
-    s = tmp.copy()
-    if len(s) != 1 and n % 2 == 1:
-        s.insert(0, 0)
-        n += 1
-    n //= 2
-print(s[0])
+card = deque()
+for i in range(1, n+1):
+    card.append(i)
+while len(card) > 1:
+    card.popleft()
+    card.append(card.popleft())
+print(card[0])
