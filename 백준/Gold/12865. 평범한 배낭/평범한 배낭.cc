371189ccb1f9f82fa425ede32d9b5677387ad860
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-05-24 11:45:54 +0900

    [Gold V] Title: 평범한 배낭, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -6,19 +6,17 @@ using namespace std;
 int n, k;
 int weight[101];
 int value[101];
-int dp[101][100001];
+int dp[100001];
 
 int main() {
 	cin >> n >> k;
 	for (int i = 1; i <= n; i++)
 		cin >> weight[i] >> value[i];
 	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
+		for (int j = k; j >= 1; j--) {
 			if (j >= weight[i])
-				dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]] + value[i]);
-			else
-				dp[i][j] = dp[i-1][j];
+				dp[j] = max(dp[j], dp[j-weight[i]] + value[i]);
 		}
 	}
-	cout << dp[n][k];
+	cout << dp[k];
 }
