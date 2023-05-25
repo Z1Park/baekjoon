fcae16bbea901f29e169c0ecde6dc773c9cc9aa5
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-05-25 10:23:23 +0900

    [Gold III] Title: 앱, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -1,23 +1,23 @@
 #include <iostream>
 #define max(a, b) ((a > b) ? a : b);
+#define FIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
 using namespace std;
 
 int main() {
-	int n, m, sum = 0, res = 10000001;
-	int memory[101];
-	int cost[101];
-	int dp[10001] = {0, };
+	int n, m, cost, sum = 0, res = 10000001;
+	int memory[101], dp[10001] = {0, };
 
-	scanf("%d %d", &n, &m);
-	for (int i = 0; i < n; i++) scanf("%d", &memory[i]);
+	FIO;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> memory[i];
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &cost[i]);
-		sum += cost[i];
-		for (int j = sum; j >= 1; j--) {
-			if (j >= cost[i]) dp[j] = max(dp[j], dp[j-cost[i]]+memory[i]);
+		cin >> cost;
+		sum += cost;
+		for (int j = sum; j >= cost; j--) {
+			dp[j] = max(dp[j], dp[j-cost]+memory[i]);
 			if (dp[j] >= m && res > j) res = j;
 		}
 	}
-	printf("%d", res);
+	cout << res;
 }
