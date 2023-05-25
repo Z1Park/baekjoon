9539854307d3edbe703869aeea9d860f8987fa1c
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-05-25 10:25:00 +0900

    [Gold III] Title: 앱, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -1,23 +1,19 @@
-#include <iostream>
+#include <stdio.h>
 #define max(a, b) ((a > b) ? a : b);
-#define FIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-
-using namespace std;
 
 int main() {
 	int n, m, cost, sum = 0, res = 10000001;
 	int memory[101], dp[10001] = {0, };
 
-	FIO;
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) cin >> memory[i];
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) scanf("%d", &memory[i]);
 	for (int i = 0; i < n; i++) {
-		cin >> cost;
+		scanf("%d", &cost);
 		sum += cost;
 		for (int j = sum; j >= cost; j--) {
 			dp[j] = max(dp[j], dp[j-cost]+memory[i]);
 			if (dp[j] >= m && res > j) res = j;
 		}
 	}
-	cout << res;
+	printf("%d", res);
 }
