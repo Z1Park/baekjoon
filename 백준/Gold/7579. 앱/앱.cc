1fc92f2a1e26f1aa6f6f9c9e7b350c3e14b4d689
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-05-25 10:15:28 +0900

    [Gold III] Title: 앱, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -1,42 +1,27 @@
 #include <iostream>
-#include <algorithm>
+#define max(a, b) ((a > b) ? a : b);
 #define FIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
 using namespace std;
 
 int n, m;
 int sum;
+int res = 10000001;
 int memory[101];
 int cost[101];
-int dp[10000001];
-
-void printDP() {
-	cout << endl;
-	for (int i = 1; i <= m; i++) {
-		cout << dp[i] << " ";
-	}
-	cout << endl;
-}
+int dp[10001];
 
 int main() {
 	FIO;
 	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-		cin >> memory[i];
+	for (int i = 0; i < n; i++) cin >> memory[i];
 	for (int i = 0; i < n; i++) {
 		cin >> cost[i];
 		sum += cost[i];
-	}
-	for (int i = 0; i < n; i++) {
 		for (int j = sum; j >= 1; j--) {
-			if (j >= cost[i])
-				dp[j] = max(dp[j], dp[j-cost[i]]+memory[i]);
+			if (j >= cost[i]) dp[j] = max(dp[j], dp[j-cost[i]]+memory[i]);
+			if (dp[j] >= m && res > j) res = j;
 		}
 	}
-	for (int i = 1; i <= sum; i++) {
-		if (dp[i] >= m) {
-			cout << i << endl;
-			break;
-		}
-	}
+	cout << res;
 }
