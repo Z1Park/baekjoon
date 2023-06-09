8a890d6f1a1feba0a74580e37f791bc91c4b7e03
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-06-09 09:46:03 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#define FIO ios::sync_with_stdio(0), cin.tie(0);
+
+using namespace std;
+
+int board[51][51];
+bool clear[51][51];
+int dr[4] = {-1, 0, 1, 0};
+int dc[4] = {0, 1, 0, -1};
+
+int main() {
+	FIO;
+
+	int n, m, r, c, d;
+	cin >> n >> m;
+	cin >> r >> c >> d;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) cin >> board[i][j];
+	}
+
+	int cnt = 0;
+	while (true) {
+		if (clear[r][c] == false) {
+			cnt++;
+			clear[r][c] = true;
+		}
+		bool flag = false;
+		for (int i = 0; i < 4; i++) {
+			d = (d + 3) % 4;
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			if (board[nr][nc] == 0 && clear[nr][nc] == false) {
+				r = nr;
+				c = nc;
+				flag = true;
+				break;
+			}
+		}
+		// for (int i = 0; i < n; i++) {
+		// 	for (int j = 0; j < m; j++) cout << clear[i][j] << " ";
+		// 	cout << endl;
+		// }
+		// cout << endl;
+		if (flag) continue;
+		int back = (d + 2) % 4;
+		if (board[r + dr[back]][c + dc[back]] == 1) break;
+		r += dr[back];
+		c += dc[back];
+	}
+	cout << cnt << endl;
+
+	return 0;
+}
