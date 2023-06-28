5a732553ab5fcb60a2b1325c4d88533dfd101b5b
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-06-28 16:32:09 +0900

    [Silver III] Title: 교수가 된 현우, Time: 32 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int tc;
+	scanf("%d", &tc);
+	while (tc--) {
+		int n;
+		scanf("%d", &n);
+		int cnt = 0;
+		int ref = 0;
+		for (int i = 5; i <= n; i *= 5) {
+			ref++;
+			cnt += n / i;
+		}
+		printf("%d\n", cnt);
+	}
+}
