620412294f818a5baafce243e218aaca6e60554a
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-10-01 08:35:14 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 102.71 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -1,20 +1,16 @@
-#include <set>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
-int getSum(vector<int>& elements, int start, int range) {
-    int sum = 0;
-    for (int i = 0; i < range; i++)
-        sum += elements[(start + i) % elements.size()];
-    return sum;
-}
-
 int solution(vector<int> elements) {
-    set<int> s;
+    unordered_set<int> us;
     for (int i = 0; i < elements.size(); i++) {
-        for (int j = 0 ; j < elements.size(); j++)
-            s.insert(getSum(elements, i, j));
+        int sum = 0;
+        for (int j = 0 ; j < elements.size(); j++) {
+            sum += elements[(i + j) % elements.size()];
+            us.insert(sum);
+        }
     }
-    return s.size();
+    return us.size();
 }
