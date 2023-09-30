58813c6573ab37984a08e8010493cf9c3397cd66
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-09-30 09:03:40 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -6,27 +6,12 @@
 using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
-    vector<int> answer;
     set<string> s;
     s.insert(words[0]);
-    int idx = 2;
-    int round = 1;
     for (int i = 1; i < words.size(); i++) {
-        auto it = s.find(words[i]);
-        if (words[i-1].back() != words[i][0] || it != s.end()) {
-            answer.push_back(idx);
-            answer.push_back(round);
-            break;
-        }
+        if (words[i-1].back() != words[i][0] || s.find(words[i]) != s.end())
+            return {i % n + 1, i / n + 1};
         s.insert(words[i]);
-        if (++idx > n) {
-            idx = 1;
-            round++;
-        }
     }
-    if (answer.empty()) {
-        answer.push_back(0);
-        answer.push_back(0);
-    }
-    return answer;
+    return {0, 0};
 }
