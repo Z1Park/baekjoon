4c143a32a33c9e4438c2fd873db1dd2e95825935
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-10-01 12:55:20 +0900

    [level 2] Title: n^2 배열 자르기, Time: 24.23 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -1,20 +1,12 @@
 #include <string>
 #include <vector>
+#define MAX(x, y) (((x) > (y) ? (x) : (y)))
 
 using namespace std;
 
 vector<int> solution(int n, long long left, long long right) {
     vector<int> answer;
-    int r = left / n, c = left % n;
-    int tmp = r + ((c > r) ? c - r : 0) + 1;
-    for (long long i = left; i <= right; i++) {
-        answer.push_back(tmp);
-        if (++c >= n) {
-            c = 0;
-            r++;
-            tmp = r + 1;
-        }
-        if (c > r) tmp++;
-    }
+    for (long long i = left; i <= right; i++)
+        answer.push_back(MAX(i % n, i / n) + 1);
     return answer;
 }
