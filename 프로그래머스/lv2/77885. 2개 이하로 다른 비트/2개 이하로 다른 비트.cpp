f1434b7632fbb91eed2da89c6134fa3a3ff885e4
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-10-06 13:39:49 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 32.46 ms, Memory: 27.7 MB -BaekjoonHub

--- 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -1,32 +1,14 @@
-#include <string>
-#include <bits/stdc++.h>
+#include <vector>
 
 using namespace std;
-
-long long f(long long number) {
-    string str;
-    bool firstZero = false;
-    while (number > 0) {
-        str.push_back(number % 2 + '0');
-        if (!firstZero && str[str.size()-1] == '0') {
-            firstZero = true;
-            str[str.size()-1] = '1';
-            if (str.size() > 1) str[str.size()-2] = '0';
-        }
-        number /= 2;
-    }
-    reverse(str.begin(), str.end());
-    if (!firstZero) {
-        str[0] = '0';
-        str = '1' + str;
-    }
-    long long res = 0;
-    for (char c : str) res = res * 2 + c - '0';
-    return res;
-}
-
+#include <iostream>
 vector<long long> solution(vector<long long> numbers) {
     vector<long long> answer;
-    for (auto number : numbers) answer.push_back(f(number));
+    for (long long num : numbers) {
+        long long bit = 1;
+        while ((num & bit) == bit) bit <<= 1;
+        num |= bit; 
+        answer.push_back((num & (~(bit >> 1))));
+    }
     return answer;
 }
