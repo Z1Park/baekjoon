b2fe6e2124a292640fdc8a118759dc4e96a58b99
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-11-10 14:32:56 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.71 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -5,8 +5,7 @@ class Solution {
         Integer[] dp = new Integer[n+1];
         dp[0] = 0;
         dp[1] = 1;
-        if (n > 1)
-            dp[2] = 2;
+        if (n > 1) dp[2] = 2;
         for (int i = 3; i <= n; i++)
             dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
         return dp[n];
