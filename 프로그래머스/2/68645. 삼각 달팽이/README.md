841c071203be794d220663349e813038cdcccbb7
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-11-15 10:04:04 +0900

    [level 2] Title: 삼각 달팽이, Time: 20.86 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/README.md
+++ 프로그래머스/2/68645. 삼각 달팽이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 삼각 달팽이 - 68645 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645?language=java#) 
 
 ### 성능 요약
 
-메모리: 129 MB, 시간: 28.36 ms
+메모리: 126 MB, 시간: 20.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 9:58:37
+2023년 11월 3일 10:4:2
 
 ### 문제 설명
 
--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -2,27 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        if (n == 1) return new int[]{1};
-        if (n == 2) return new int[]{1, 2, 3};
         int[][] arr = new int[n][];
         for (int i = 0; i < n; i++) {
             arr[i] = new int[i+1];
             for (int j = 0; j <= i; j++) arr[i][j] = -1;
         }
-        int r = 0, c = 0, curr = 1;
-        while (arr[r][c] == -1) {
-            while (r < n && arr[r][c] == -1)
-                arr[r++][c] = curr++;
-            r -= 1;
-            c += 1;
-            while (c < arr[r].length && arr[r][c] == -1)
-                arr[r][c++] = curr++;
-            c -= 2;
-            r -= 1;
-            while (arr[r][c] == -1)
-                arr[r--][c--] = curr++;
-            r += 2;
-            c += 1;
+        int r = -1, c = 0, curr = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (i % 3 == 0) ++r;
+                else if (i % 3 == 1) ++c;
+                else {--r; --c;}
+                arr[r][c] = curr++;
+            }
         }
         return Arrays.stream(arr).flatMapToInt(Arrays::stream).toArray();
     }
