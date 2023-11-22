71319d06c390494f5177189a89d9101216a5dc4b
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-11-22 16:57:32 +0900

    [level 3] Title: 입국심사, Time: 0.79 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,24 +1,6 @@
-import java.util.*;
-
 class Solution {
-    
-    private long getCount(int[] times, long curr) {
-        long count = 0;
-        for (int time : times)
-            count += curr / (long)time;
-        return count;
-    }
-    
     public long solution(int n, int[] times) {
-        int max = Arrays.stream(times).max().getAsInt();
-        long start = 0, end = (long)max * n;
-        while (start <= end) {
-            long mid = (start / 2 + end / 2) + 
-                ((start % 2 == 1 && end % 2 == 1) ? 1 : 0);
-            if (getCount(times, mid) < n)
-                start = mid + 1;
-            else end = mid - 1;
-        }
-        return start;
+        long answer = 0;
+        return answer;
     }
 }
