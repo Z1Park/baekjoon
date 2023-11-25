3bf5d669e73ebfe4b537ae4f1c93e2082b64d05f
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-11-25 20:20:47 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(String[] board) {
         int[] count = new int[2];
-        int[] check = new int[2];
+        boolean[] check = new boolean[2];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board[i].charAt(j) == 'O') count[0]++;
@@ -12,34 +12,33 @@ class Solution {
             if (board[i].charAt(0) != '.' 
                 && board[i].charAt(0) == board[i].charAt(1)
                 && board[i].charAt(0) == board[i].charAt(2)) {
-                if (board[i].charAt(0) == 'O') check[0]++;
-                else check[1]++;
+                if (board[i].charAt(0) == 'O') check[0] = true;
+                else check[1] = true;
             }
             if (board[0].charAt(i) != '.' 
                 && board[0].charAt(i) == board[1].charAt(i)
                 && board[1].charAt(i) == board[2].charAt(i)) {
-                if (board[0].charAt(i) == 'O') check[0]++;
-                else check[1]++;
+                if (board[0].charAt(i) == 'O') check[0] = true;
+                else check[1] = true;
             }
         }
         if (board[0].charAt(0) != '.' 
             && board[0].charAt(0) == board[1].charAt(1)
             && board[1].charAt(1) == board[2].charAt(2)) {
-            if (board[0].charAt(0) == 'O') check[0]++;
-            else check[1]++;
+            if (board[0].charAt(0) == 'O') check[0] = true;
+            else check[1] = true;
         }
         if (board[0].charAt(2) != '.' 
             && board[0].charAt(2) == board[1].charAt(1)
             && board[1].charAt(1) == board[2].charAt(0)) {
-            if (board[1].charAt(1) == 'O') check[0]++;
-            else check[1]++;
+            if (board[1].charAt(1) == 'O') check[0] = true;
+            else check[1] = true;
         }
-        System.out.println(check[0] + " " + check[1]);
         if (count[0] < count[1]) return 0;
         if (Math.abs(count[0] - count[1]) > 1) return 0;
-        if (check[0] != 0 && check[1] != 0) return 0;
-        if (check[0] != 0 && count[0] == count[1]) return 0;
-        if (check[1] != 0 && count[0] > count[1]) return 0;
+        if (check[0] && check[1]) return 0;
+        if (check[0] && count[0] == count[1]) return 0;
+        if (check[1] && count[0] > count[1]) return 0;
         return 1;
     }
 }
