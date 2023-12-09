2e309b7a2ad0fcb995c67cb64f201b9d57a2fd69
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-12-09 21:14:39 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.14 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -7,19 +7,15 @@ class Solution {
     
     public int solution(String s) {
         int cnt = 0;
-        for (int i = 0; i < s.length(); i++) {
-            cnt++;
+        for (int i = 0; i < s.length(); i++, cnt++) {
             Deque<Character> stk = new ArrayDeque<>();
             for (int idx = 0; idx < s.length(); idx++) {
                 char c = s.charAt((idx + i) % s.length());
-                if (open.indexOf(c) != -1)
-                    stk.addLast(c);
-                else {
-                    if (stk.isEmpty() || closed.indexOf(c) != open.indexOf(stk.pollLast())) {
-                        cnt--;
-                        stk.clear();
-                        break;
-                    }
+                if (open.indexOf(c) != -1) stk.addLast(c);
+                else if (stk.isEmpty() || closed.indexOf(c) != open.indexOf(stk.pollLast())) {
+                    cnt--;
+                    stk.clear();
+                    break;
                 }
             }
             if (!stk.isEmpty()) cnt--;
