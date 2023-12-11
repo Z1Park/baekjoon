68d9aa8b3853695c9a29e20922065a938a968b1c
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-12-12 08:48:13 +0900

    [level 2] Title: 귤 고르기, Time: 60.69 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,18 +1,19 @@
 import java.util.*;
 import java.util.stream.*;
+import static java.util.stream.Collectors.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int t : tangerine)
             map.put(t, map.getOrDefault(t, 0) + 1);
-        List<Integer> list = map.values().stream()
-            .sorted((a, b) -> b - a).collect(Collectors.toList());
+        List<Integer> list = map.values().stream().
+            sorted((a, b) -> b - a).collect(toList());
         int cnt = 0, sum = 0;
         for (int e : list) {
             sum += e;
             cnt++;
-            if (k <= sum) break;
+            if (sum >= k) break;
         }
         return cnt;
     }
