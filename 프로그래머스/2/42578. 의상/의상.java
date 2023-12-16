15eb3ba35e250f83153edeb74a8dc6b877b14857
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-12-16 19:08:05 +0900

    [level 2] Title: 의상, Time: 1.01 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,13 +1,12 @@
 import java.util.*;
-import static java.util.stream.Collectors.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        return Arrays.stream(clothes)
-            .collect(groupingBy(cloth -> cloth[1], 
-                               mapping(elem -> elem, counting())))
-            .values()
-            .stream()
-            .collect(reducing(1L, (a, b) -> a * (b + 1))).intValue() - 1;
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] cloth : clothes)
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+        return map.values().stream()
+            .map(a -> a + 1).reduce((a, b) -> a * b).orElse(0) - 1;
     }
 }
