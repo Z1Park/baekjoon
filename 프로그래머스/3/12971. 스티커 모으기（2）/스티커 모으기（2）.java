c2ad8062f5fd5839284440eff3587cdefe8bc6d8
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2023-12-24 11:07:45 +0900

    [level 3] Title: 스티커 모으기(2), Time: 6.73 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,20 +1,20 @@
 import java.util.*;
 
 class Solution {
+    
     public int solution(int sticker[]) {
-        if (sticker.length == 1) return sticker[0];
-        if (sticker.length == 2) return Math.max(sticker[0], sticker[1]);
-        
-        int[] dp1 = new int[sticker.length - 1];
+        int N = sticker.length;
+        if (N == 1) return sticker[0];
+        int[] dp1 = new int[N];
         dp1[0] = sticker[0];
         dp1[1] = Math.max(dp1[0], sticker[1]);
-        for (int i = 2; i < dp1.length; i++)
+        for (int i = 2; i < N-1; i++)
             dp1[i] = Math.max(dp1[i-1], dp1[i-2] + sticker[i]);
-        int[] dp2 = new int[sticker.length];
+        int[] dp2 = new int[N];
         dp2[0] = 0;
         dp2[1] = sticker[1];
-        for (int i = 2; i < dp2.length; i++)
+        for (int i = 2; i < N; i++)
             dp2[i] = Math.max(dp2[i-1], dp2[i-2] + sticker[i]);
-        return Math.max(dp1[dp1.length-1], dp2[dp2.length-1]);
+        return Math.max(dp1[N-2], dp2[N-1]);
     }
 }
