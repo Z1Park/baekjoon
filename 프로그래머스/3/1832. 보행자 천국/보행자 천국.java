e913e32b17b9952817d2d925672563b1109e0690
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-01-02 18:38:45 +0900

    [level 3] Title: 보행자 천국, Time: 255.54 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -7,57 +7,35 @@ class Solution {
     private final int[] dc = {1, 0};
     
     public int solution(int R, int C, int[][] cityMap) {
-        boolean[][][] visit = new boolean[R][C][2];
-        int[][][] dp = new int[R][C][2];
-        Deque<int[]> dq = new ArrayDeque<>();
+        boolean[][] visit = new boolean[R][C];
+        int[][] dp = new int[R][C];
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            Comparator.comparing(arr -> arr[3]));
         
-        visit[0][0][0] = true;
-        visit[0][0][1] = true;
-        dp[0][0][0] = 1;
-        dq.add(new int[]{0, 0, -1});
-        while (!dq.isEmpty()) {
-            int[] tmp = dq.poll();
+        visit[0][0] = true;
+        dp[0][0] = 1;
+        pq.add(new int[]{0, 0, -1, 0});
+        while (!pq.isEmpty()) {
+            int[] tmp = pq.poll();
             int r = tmp[0], c = tmp[1], dir = tmp[2];
             for (int i = 0; i < 2; i++) {
-                if (cityMap[r][c] == 2 && dir != i) continue;
+                int cnt = tmp[3];
                 int nr = r + dr[i], nc = c + dc[i];
-                if (0 <= nr && nr < R && 0 <= nc && nc < C) {
-                    if (cityMap[nr][nc] == 1) continue;
-                    if (cityMap[r][c] == 0) {
-                        if (cityMap[nr][nc] == 0) {
-                            dp[nr][nc][0] = (dp[nr][nc][0] + dp[r][c][0]) % MOD;
-                            if (!visit[nr][nc][0]) {
-                                visit[nr][nc][0] = true;
-                                dq.addLast(new int[]{nr, nc, i});
-                            }
-                        }
-                        else {
-                            dp[nr][nc][i] = (dp[nr][nc][i] + dp[r][c][0]) % MOD;
-                            if (!visit[nr][nc][i]) {
-                                visit[nr][nc][i] = true;
-                                dq.addLast(new int[]{nr, nc, i});
-                            }
-                        }
-                    }
-                    else if (cityMap[r][c] == 2) {
-                        if (cityMap[nr][nc] == 0) {
-                            dp[nr][nc][0] = (dp[nr][nc][0] + dp[r][c][i]) % MOD;
-                            if (!visit[nr][nc][0]) {
-                                visit[nr][nc][0] = true;
-                                dq.addLast(new int[]{nr, nc, i});
-                            }
-                        }
-                        else {
-                            dp[nr][nc][i] = (dp[nr][nc][i] + dp[r][c][i]) % MOD;
-                            if (!visit[nr][nc][i]) {
-                                visit[nr][nc][i] = true;
-                                dq.addLast(new int[]{nr, nc, i});
-                            }
-                        }
-                    }
+                while (0 <= nr && nr < R && 0 <= nc && nc < C && cityMap[nr][nc] == 2) {
+                    nr += dr[i];
+                    nc += dc[i];
+                    cnt++;
+                }
+                if (nr < 0 || R <= nr || nc < 0 || C <= nc)
+                    continue;
+                if (cityMap[nr][nc] == 1) continue;
+                dp[nr][nc] = (dp[nr][nc] + dp[r][c]) % MOD;
+                if (!visit[nr][nc]) {
+                    visit[nr][nc] = true;
+                    pq.add(new int[]{nr, nc, i, cnt+1});
                 }
             }
         }
-        return dp[R-1][C-1][0];
+        return dp[R-1][C-1];
     }
 }
