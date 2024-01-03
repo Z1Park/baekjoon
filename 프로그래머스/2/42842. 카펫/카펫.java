f8d14e44285688c9edb4fcff0004de3a869214f0
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-01-04 08:56:56 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,14 @@
-// brown = 2 * (a + b) - 4
-// yellow = (a - 2) * (b - 2)
+// brown = 2 * (width + height) - 4
+// yellow = (width - 2) * (heigth - 2)
+
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int apb = (brown + 4) / 2;
-        int ab = yellow + 2 * apb - 4;
-        int h = 3, w = apb - h;
-        while (w >= h) {
-            if (w*h == ab) {
-                int[] res = new int[]{w, h};
-                return res;
-            }
-            h++;
-            w = apb - h;
+        for (int i = 1; i <= yellow; i++) {
+            if (yellow % i != 0) continue;
+            int height = i + 2, width = yellow / i + 2;
+            if (2 * (width + height) - 4 == brown)
+                return new int[]{width, height};
         }
-        return new int[]{1, 1};
+        return new int[]{-1, -1};
     }
 }
