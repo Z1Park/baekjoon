5086575d0359b73f3e0b14e88436d008affe76bd
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-01-05 16:18:54 +0900

    [level 3] Title: 섬 연결하기, Time: 4.38 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 2.18 ms
+메모리: 76.1 MB, 시간: 4.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 1일 17:10:18
+2024년 1월 5일 16:18:51
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,37 +2,33 @@ import java.util.*;
 
 class Solution {
     
-    private List<List<Integer>> graph = new ArrayList<>();
+    private int[] parent;
     
-    private boolean checkConnect(int n, int a, int b) {
-        boolean[] visit = new boolean[n];
-        Deque<Integer> dq = new ArrayDeque<>();
-        dq.add(a);
-        while (!dq.isEmpty()) {
-            int node = dq.poll();
-            if (node == b) return true;
-            if (visit[node]) continue;
-            visit[node] = true;
-            List<Integer> nextNodes = graph.get(node);
-            for (int next : nextNodes) {
-                if (!visit[next]) dq.addLast(next);
-            }
-        }
-        return false;
+    private int find(int x) {
+        if (parent[x] == x) return x;
+        return find(parent[x]);
+    }
+    
+    private void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        
+        if (x == y) return;
+        if (x < y) parent[y] = x;
+        else parent[x] = y;
     }
     
     public int solution(int n, int[][] costs) {
-        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+        parent = new int[n];
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
         Arrays.sort(costs, Comparator.comparing(arr -> arr[2]));
-        int sum = 0, cnt = 0;
+        int sum = 0;
         for (int[] cost : costs) {
-            if (!checkConnect(n, cost[0], cost[1])) {
+            if (find(cost[0]) != find(cost[1])) {
+                union(cost[0], cost[1]);
                 sum += cost[2];
-                graph.get(cost[0]).add(cost[1]);
-                graph.get(cost[1]).add(cost[0]);
-                cnt++;
             }
-            if (cnt >= n - 1) break;
         }
         return sum;
     }
