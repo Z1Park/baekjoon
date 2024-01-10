af980977ad93a5389ecb37d86958e0002f06f473
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-01-10 20:17:59 +0900

    [level 3] Title: 등굣길, Time: 0.99 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,27 +1,21 @@
-import java.util.*;
-
 class Solution {
     
-    private static final int mod = 1000000007;
+    private static final int MOD = 1000000007;
     
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[n+1][m+1];
-        for (int[] p : puddles) dp[p[1]][p[0]] = -1;
-        dp[1][1] = 1;
-        for (int r = 1; r <= n; r++) {
-            for (int c = 1; c <= m; c++) {
+        int[][] dp = new int[n][m];
+        dp[0][0] = 1;
+        for (int[] p : puddles) dp[p[1]-1][p[0]-1] = -1;
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
                 if (dp[r][c] == -1) continue;
                 int nr = r + 1, nc = c + 1;
-                if (nr <= n && dp[nr][c] != -1) {
-                    dp[nr][c] += dp[r][c];
-                    if (dp[nr][c] > mod) dp[nr][c] %= mod;
-                }
-                if (nc <= m && dp[r][nc] != -1) {
-                    dp[r][nc] += dp[r][c];
-                    if (dp[r][nc] > mod) dp[r][nc] %= mod;
-                }
+                if (nr < n && dp[nr][c] != -1) 
+                    dp[nr][c] = (dp[nr][c] + dp[r][c]) % MOD;
+                if (nc < m && dp[r][nc] != -1) 
+                    dp[r][nc] = (dp[r][nc] + dp[r][c]) % MOD;
             }
         }
-        return dp[n][m];
+        return dp[n-1][m-1];
     }
 }
