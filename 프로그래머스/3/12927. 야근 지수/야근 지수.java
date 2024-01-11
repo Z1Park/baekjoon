7f580ccc9373696f8bbd9527e3305f9107d6e329
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-01-11 10:50:45 +0900

    [level 3] Title: 야근 지수, Time: 118.77 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -5,28 +5,18 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for (int w : works) pq.add(w);
         while (n > 0 && pq.peek() != 0) {
-            int tmp = pq.poll();
-            int gap = tmp - pq.peek();
-            if (gap < n) {
-                if (tmp - gap == 0) {
-                    pq.add(0);
-                    n -= gap;
-                }
-                else {
-                    pq.add(tmp - gap - 1);
-                    n -= gap + 1;
-                }
-            }
-            else {
-                pq.add(tmp - n);
-                n = 0;
-            }
+            int currWork = pq.poll();
+            int gap = currWork - pq.peek();
+            if (gap >= n) gap = n;
+            else if (currWork - gap > 0) gap++;
+            pq.add(currWork - gap);
+            n -= gap;
         }
-        long answer = 0;
+        long sum = 0;
         while (!pq.isEmpty()) {
             int tmp = pq.poll();
-            answer += tmp * tmp;
+            sum += tmp * tmp;
         }
-        return answer;
+        return sum;
     }
 }
