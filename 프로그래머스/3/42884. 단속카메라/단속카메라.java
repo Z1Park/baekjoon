523a50bec1c8cd7b1569038567e3ce905c5c07c7
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-01-24 11:59:34 +0900

    [level 3] Title: 단속카메라, Time: 14.65 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        Arrays.sort(routes, Comparator.comparing(arr -> arr[1]));
-        int cnt = 0, lastCam = -30001;
-        for (int[] e : routes) {
-            if (lastCam < e[0]) {
+        Arrays.sort(routes, Comparator.comparing((int[] arr) -> arr[1]));
+        int cnt = 0, last = -30001;
+        for (int[] route : routes) {
+            if (last < route[0]) {
+                last = route[1];
                 cnt++;
-                lastCam = e[1];
             }
         }
         return cnt;
