941d1497be0786708c46be2d9042ec245df66315
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-02-19 23:31:23 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.10 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/README.md
+++ 프로그래머스/2/152996. 시소 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [level 2] 시소 짝꿍 - 152996 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152996#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152996) 
 
 ### 성능 요약
 
-메모리: 66.1 MB, 시간: 0.10 ms
+메모리: 77.6 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 11:20:42
+2024년 2월 1일 23:31:20
 
 ### 문제 설명
 
--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -2,25 +2,21 @@ import java.util.*;
 
 class Solution {
     public long solution(int[] weights) {
-        int maxWeight = 0;
+        int[] realWeight = new int[1001];
+        int[] distWeight = new int[4001];
+        long cnt = 0;
         for (int weight : weights) {
-            if (maxWeight < weight) maxWeight = weight;
-        }
-        int[] realWeights = new int[maxWeight + 1];
-        int[] relationWeights = new int[4 * maxWeight + 1];
-        for (int i = 0; i < relationWeights.length; i++) {
-            if (i < realWeights.length) realWeights[i] = -1;
-            relationWeights[i] = -1;
-        }
-        long res = 0;
-        for (int weight : weights) {
-            realWeights[weight]++;
-            res += realWeights[weight];
+            int tmp = 0;
             for (int i = 2; i <= 4; i++) {
-                relationWeights[weight * i]++;
-                res += relationWeights[weight * i] - realWeights[weight];
+                int curr = weight * i;
+                tmp += distWeight[curr];
+                distWeight[curr] += 1;
+                tmp -= realWeight[weight];
             }
+            tmp += realWeight[weight];
+            realWeight[weight] += 1;
+            cnt += tmp;
         }
-        return res;
+        return cnt;
     }
 }
