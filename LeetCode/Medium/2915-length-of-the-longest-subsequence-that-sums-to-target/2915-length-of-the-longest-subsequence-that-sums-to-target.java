3389e829bf09353c57d35182e658bcf8d8582cb1
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-02-28 19:05:02 +0900

    Time: 33 ms (86.07%), Space: 44.3 MB (90.57%) - LeetHub

--- LeetCode/Medium/2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
+++ LeetCode/Medium/2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
@@ -1,14 +1,13 @@
 class Solution {
     public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
         int[] dp = new int[target+1];
-        Arrays.fill(dp, -1);
-        dp[0] = 0;
+        dp[0] = 1;
         for (int num : nums) {
             for (int j = target; j >= 1; j--) {
-                if (j < num || dp[j-num] == -1) continue;
+                if (j < num || dp[j-num] == 0) continue;
                 dp[j] = Math.max(dp[j], dp[j-num]+1);
             }
         }
-        return dp[target];
+        return dp[target]-1;
     }
 }
