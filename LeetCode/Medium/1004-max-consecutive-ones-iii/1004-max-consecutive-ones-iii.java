d0b0af5ee2da1013a9457759ef8f0f62bc4465d3
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-02-29 16:42:29 +0900

    Time: 3 ms (78.88%), Space: 46.9 MB (71.69%) - LeetHub

--- LeetCode/Medium/1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ LeetCode/Medium/1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,30 +1,26 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int maxValue = 0, count = 0;
-        if (k == 0) {
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == 0) count = 0;
-                else count++;
-                if (maxValue < count)
-                    maxValue = count;
-            }
-            return maxValue;
-        }
-        
-        Queue<Integer> que = new ArrayDeque<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                if (que.size() >= k) {
-                    int front = que.poll();
-                    count = i - front - 1;
-                }
-                que.add(i);
+        int l = 0, r = 0, count = 0, maxValue = 0;
+        while (r < nums.length) {
+            if (nums[r] == 1) {
                 count++;
+                r++;
             }
             else {
-                count++;
+                if (k > 0) {
+                    r++;
+                    k--;
+                    count++;
+                }
+                else {
+                    while (nums[l] == 1)
+                        l++;
+                    l++;
+                    r++;
+                    count = r - l;
+                }
             }
-            if (count > maxValue)
+            if (maxValue < count)
                 maxValue = count;
         }
         return maxValue;
