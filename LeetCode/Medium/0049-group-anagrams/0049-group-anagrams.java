4e0284dd35f1cf952098b3d4be3fa230d0fb5564
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-03 20:59:22 +0900

    Time: 7 ms (64.31%), Space: 47.5 MB (67.09%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
@@ -3,9 +3,11 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> res = new HashMap<>();
         for (String str : strs) {
-            char[] arr = str.toCharArray();
-            Arrays.sort(arr);
-            res.computeIfAbsent(String.valueOf(arr), k -> new ArrayList<>()).add(str);
+            char[] alps = new char[26];
+            for (int i = 0; i < str.length(); i++)
+                alps[str.charAt(i)-'a']++;
+            res.computeIfAbsent(String.valueOf(alps), k -> new ArrayList<>())
+                .add(str);
         }
         return res.values().stream().toList();
     }
