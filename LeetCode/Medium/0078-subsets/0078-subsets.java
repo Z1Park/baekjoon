266b9ccebf1ac28dbdbc713867a0986135f26427
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-03 18:15:21 +0900

    Time: 1 ms (64.24%), Space: 42.7 MB (36.41%) - LeetHub

--- LeetCode/Medium/0078-subsets/0078-subsets.java
+++ LeetCode/Medium/0078-subsets/0078-subsets.java
@@ -2,23 +2,19 @@ class Solution {
     
     private List<List<Integer>> combinations = new ArrayList<>();
     
-    private void getCombination(int[] nums, List<Integer> buffer, int len, int curr) {
-        if (buffer.size() >= len) {
-            combinations.add(List.copyOf(buffer));
-            return;
-        }
+    private void getCombination(int[] nums, List<Integer> buffer, int curr) {
+        combinations.add(List.copyOf(buffer));
+        
         for (int i = curr; i < nums.length; i++) {
             buffer.add(nums[i]);
-            getCombination(nums, buffer, len, i+1);
+            getCombination(nums, buffer, i+1);
             buffer.remove(buffer.size()-1);
         }
     }
     
     public List<List<Integer>> subsets(int[] nums) {
         List<Integer> buffer = new ArrayList<>();
-        
-        for (int i = 0; i <= nums.length; i++)
-            getCombination(nums, buffer, i, 0);
+        getCombination(nums, buffer, 0);
         return combinations;
     }
 }
