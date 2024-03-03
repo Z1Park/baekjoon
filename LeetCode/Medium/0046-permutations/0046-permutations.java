98ed7d1d671fcc059dab18df126076ecae4652bb
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-03 18:05:57 +0900

    Time: 1 ms (95.51%), Space: 44.5 MB (34.89%) - LeetHub

--- LeetCode/Medium/0046-permutations/0046-permutations.java
+++ LeetCode/Medium/0046-permutations/0046-permutations.java
@@ -2,22 +2,24 @@ class Solution {
     
     private List<List<Integer>> permutations = new ArrayList<>();
     
-    private void getPermutation(Set<Integer> used, int[] nums) {
-        if (used.size() >= nums.length) {
-            permutations.add(new ArrayList<>(used));
+    private void getPermutation(boolean[] used, List<Integer> list, int[] nums) {
+        if (list.size() >= nums.length) {
+            permutations.add(List.copyOf(list));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (!used.contains(nums[i])) {
-                used.add(nums[i]);
-                getPermutation(used, nums);
-                used.remove(nums[i]);
+            if (!used[i]) {
+                used[i] = true;
+                list.add(nums[i]);
+                getPermutation(used, list, nums);
+                list.remove(list.size()-1);
+                used[i] = false;
             }
         }
     }
     
     public List<List<Integer>> permute(int[] nums) {
-        getPermutation(new LinkedHashSet<>(), nums);
+        getPermutation(new boolean[nums.length], new ArrayList<>(), nums);
         return permutations;
     }
 }
