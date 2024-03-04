ef047f7952d4ba4d2367ca455192e14d54af0a12
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-04 19:02:50 +0900

    Time: 28 ms (5.30%), Space: 73.5 MB (5.01%) - LeetHub

---
+++ LeetCode/Medium/0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,45 @@
+class Solution {
+    
+    private List<List<Integer>> combinations = new ArrayList<>(); 
+    private Deque<Integer> buffer = new ArrayDeque<>();
+    
+    
+    private boolean isPalindrome(String str) {
+        for (int i = 0, j = str.length()-1; i < str.length() / 2; i++, j--) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+        }
+        return true;
+    }
+    
+    private void makeCombination(String s, int idx) {
+        if (idx >= s.length()) {
+            combinations.add(new ArrayList<>(buffer));
+            return;
+        }
+        
+        for (int i = idx+1; i <= s.length(); i++) {
+            String partition = s.substring(idx, i);
+            if (isPalindrome(partition)) {
+                buffer.add(i);
+                makeCombination(s, i);
+                buffer.pollLast();
+            }
+        }
+    }
+    
+    public List<List<String>> partition(String s) {
+        makeCombination(s, 0);
+        List<List<String>> result = new ArrayList<>();
+        for (List<Integer> combination : combinations) {
+            int idx = 0;
+            List<String> tmp = new ArrayList<>();
+            for (int i : combination) {
+                tmp.add(s.substring(idx, i));
+                idx = i;
+            }
+            result.add(tmp);
+        }
+        return result;
+    }
+}
