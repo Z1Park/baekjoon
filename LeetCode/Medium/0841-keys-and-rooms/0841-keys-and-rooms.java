bf25ac7469407ee82efc50cd05767f13c02d79c1
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-06 18:08:27 +0900

    Time: 1 ms (79.13%), Space: 44.7 MB (5.29%) - LeetHub

--- LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.java
+++ LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,21 +1,20 @@
 class Solution {
+    
+    private int cnt = 0;
+    private boolean[] used;
+    
+    private void dfs(List<List<Integer>> rooms, int curr) {
+        if (used[curr]) return;
+        used[curr] = true;
+        cnt++;
+        for (int next : rooms.get(curr))
+            dfs(rooms, next);
+    }
+    
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         Deque<Integer> keys = new ArrayDeque<>();
-        boolean[] used = new boolean[rooms.size()];
-        int cnt = 1;
-        
-        keys.add(0);
-        used[0] = true;
-        while (!keys.isEmpty()) {
-            int key = keys.poll();
-            List<Integer> newKeys = rooms.get(key);
-            for (int newKey : newKeys) {
-                if (used[newKey]) continue;
-                keys.add(newKey);
-                used[newKey] = true;
-                cnt++;
-            }
-        }
+        used = new boolean[rooms.size()];
+        dfs(rooms, 0);
         return cnt == rooms.size();
     }
 }
