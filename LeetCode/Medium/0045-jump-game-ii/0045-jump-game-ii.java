124fc883c2cb44a4e4a1eea6b6a89f94a234fa50
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-07 16:33:22 +0900

    Time: 2 ms (77.11%), Space: 44.6 MB (93.05%) - LeetHub

--- LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.java
+++ LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.java
@@ -2,14 +2,13 @@ class Solution {
     public int jump(int[] nums) {
         if (nums.length == 1) return 0;
         int maxReach = 0, current = 0, count = 0;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             maxReach = Math.max(maxReach, i + nums[i]);
             if (i == current) {
                 current = maxReach;
                 count++;
-                if (current >= nums.length-1) return count;
             }
         }
-        return count-1;
+        return count;
     }
 }
