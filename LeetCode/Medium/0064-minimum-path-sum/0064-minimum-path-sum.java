27cdc7c17d8e6aafe046278ab57fed0cf31bf930
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-07 19:24:59 +0900

    Time: 1 ms (95.86%), Space: 45.3 MB (78.61%) - LeetHub

--- LeetCode/Medium/0064-minimum-path-sum/0064-minimum-path-sum.java
+++ LeetCode/Medium/0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,19 +1,18 @@
 class Solution {
     
+    private int reverseDFS(int r, int c, int[][] grid, int[][] dp) {
+        if (r == 0 && c == 0) return grid[0][0];
+        if (r < 0 || c < 0) return Integer.MAX_VALUE;
+        if (dp[r][c] != 0) return dp[r][c];
+        int left = reverseDFS(r, c-1, grid, dp);
+        int up = reverseDFS(r-1, c, grid, dp);
+        dp[r][c] = grid[r][c] + Math.min(left, up);
+        return dp[r][c];
+    }
+    
     public int minPathSum(int[][] grid) {
         int R = grid.length, C = grid[0].length;
         int[][] dp = new int[R][C];
-        for (int i = 0; i < R; i++)
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
-        dp[0][0] = grid[0][0];
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                if (i != R-1)
-                    dp[i+1][j] = Math.min(dp[i+1][j], dp[i][j] + grid[i+1][j]);
-                if (j != C-1)
-                    dp[i][j+1] = Math.min(dp[i][j+1], dp[i][j] + grid[i][j+1]);
-            }
-        }
-        return dp[R-1][C-1];
+        return reverseDFS(R-1, C-1, grid, dp);
     }
 }
