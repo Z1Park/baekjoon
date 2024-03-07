025c831555ca03b768340ab7eae2114e88563c0f
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-07 16:23:42 +0900

    Time: 40 ms (19.57%), Space: 45.9 MB (7.79%) - LeetHub

--- LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.java
+++ LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.java
@@ -3,19 +3,12 @@ class Solution {
         int[] dp = new int[nums.length];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        pq.add(new int[]{0, 0});
-        while (!pq.isEmpty()) {
-            int[] tmp = pq.poll();
-            int i = tmp[0], count = tmp[1];
-            if (i == nums.length-1) break;
+        for (int i = 0; i < nums.length; i++) {
             for (int j = 1; j <= nums[i]; j++) {
                 int idx = i + j;
                 if (idx >= nums.length) break;
-                if (count + 1 < dp[idx]) {
-                    dp[idx] = Math.min(dp[idx], count + 1);
-                    pq.add(new int[]{idx, count+1});
-                }
+                if (idx == nums.length-1) return dp[i]+1;
+                dp[idx] = Math.min(dp[idx], dp[i] + 1);
             }
         }
         return dp[nums.length-1];
