515cebd7c805c9a2d0161bea936065a403796404
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-19 20:09:32 +0900

    Time: 4 ms (96.51%), Space: 44.7 MB (69.98%) - LeetHub

--- LeetCode/Medium/0155-min-stack/0155-min-stack.java
+++ LeetCode/Medium/0155-min-stack/0155-min-stack.java
@@ -1,33 +1,33 @@
 class MinStack {
     
-    private TreeMap<Integer, Integer> orderedMap = new TreeMap<>();
     private Deque<Integer> stk = new ArrayDeque<>();
+    private int min = Integer.MAX_VALUE;
 
     public MinStack() {
     }
     
     public void push(int val) {
         stk.add(val);
-        orderedMap.put(val, orderedMap.getOrDefault(val, 0) + 1);
+        if (min > val) min = val;
     }
     
     public void pop() {
         int res = stk.pollLast();
-        while (!orderedMap.containsKey(res))
-            res = stk.pollLast();
-        int count = orderedMap.get(res);
-        if (count == 1) orderedMap.remove(res);
-        else orderedMap.put(res, count - 1);
+        if (min == res) {
+            int nextMin = Integer.MAX_VALUE;
+            for (int e : stk) {
+                if (nextMin > e) nextMin = e;
+            }
+            min = nextMin;
+        }
     }
     
     public int top() {
-        while (!orderedMap.containsKey(stk.peekLast()))
-            stk.pollLast();
         return stk.peekLast();
     }
     
     public int getMin() {
-        return orderedMap.firstEntry().getKey();
+        return min;
     }
 }
 
