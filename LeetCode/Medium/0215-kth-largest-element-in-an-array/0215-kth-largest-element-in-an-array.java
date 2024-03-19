b16ef0a99a8396009ba08964e7f7ef547b3af98d
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-19 10:56:20 +0900

    Time: 3 ms (99.60%), Space: 54.8 MB (99.48%) - LeetHub

--- LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ LeetCode/Medium/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,12 +1,14 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int[] counts = new int[20002];
-        int offset = 10000;
-        for (int i = 0; i < nums.length; i++)
-            counts[offset + nums[i]]++;
-        int i = counts.length-1;
-        for (; k > 0; i--)
+        int[] counts = new int[20001];
+        int offset = 10_000;
+        
+        for (int num : nums)
+            counts[num + offset]++;
+        for (int i = counts.length-1; i >= 0; i--) {
             k -= counts[i];
-        return i - offset + 1;
+            if (k <= 0) return i - offset;
+        }
+        return 0;
     }
 }
