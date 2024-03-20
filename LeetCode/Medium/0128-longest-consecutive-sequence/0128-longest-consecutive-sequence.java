8f1dac2b2787657d68016fe3738bcb97ea3d1699
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-20 17:03:53 +0900

    Time: 4 ms (99.03%), Space: 57.5 MB (71.75%) - LeetHub

--- LeetCode/Medium/0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ LeetCode/Medium/0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,13 +1,33 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         if (nums.length == 0) return 0;
-        Arrays.sort(nums);
-        int maxCount = 1, count = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i-1]+1) count++;
-            else if (nums[i] - nums[i-1] > 1) count = 1;
-            if (maxCount < count) maxCount = count;
+        if (nums.length < 1000) {
+            Arrays.sort(nums);
+            int maxCount = 1, count = 1;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] == nums[i-1]+1) count++;
+                else if (nums[i] - nums[i-1] > 1) count = 1;
+                if (maxCount < count) maxCount = count;
+            }
+            return maxCount;
         }
-        return maxCount;
+        else {
+            int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+            for (int i = 0; i < nums.length; i++) {
+                if (min > nums[i]) min = nums[i];
+                if (max < nums[i]) max = nums[i];
+            }
+            
+            boolean[] exist = new boolean[max - min + 1];
+            for (int i = 0; i < nums.length; i++)
+                exist[nums[i]-min] = true;
+            int maxCount = 0, count = 0;
+            for (int i = 0; i < max - min + 1; i++) {
+                if (exist[i]) count++;
+                else count = 0;
+                if (maxCount < count) maxCount = count;
+            }
+            return maxCount;
+        }
     }
 }
