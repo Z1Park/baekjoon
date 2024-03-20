7a804108e3314e6410d62212ebe3d89b5b6303b5
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-20 16:27:04 +0900

    Time: 10 ms (80.57%), Space: 44.7 MB (72.46%) - LeetHub

--- LeetCode/Medium/0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ LeetCode/Medium/0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -8,12 +8,17 @@ class Solution {
         
         List<Integer> result = new ArrayList<>();
         char[] window = new char[26];
-        for (int i = 0; i < s.length(); i++) {
-            window[s.charAt(i) - 'a']++;
-            if (i >= p.length()) window[s.charAt(i-p.length()) - 'a']--;
+        int l = 0, r = 0;
+        for (; r < p.length(); r++)
+            window[s.charAt(r) - 'a']++;
+        for (; r < s.length(); l++, r++) {
             if (Arrays.equals(ref, window))
-                result.add(i - p.length() + 1);
+                result.add(l);
+            window[s.charAt(r) - 'a']++;
+            window[s.charAt(l) - 'a']--;
         }
+        if (Arrays.equals(ref, window))
+            result.add(s.length() - p.length());
         return result;
     }
 }
