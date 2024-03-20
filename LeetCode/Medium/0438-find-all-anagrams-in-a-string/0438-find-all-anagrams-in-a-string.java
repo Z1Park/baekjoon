59a0615eb5f32fb03c171ebc98f393aac3cd0c9b
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-20 16:21:33 +0900

    Time: 13 ms (60.90%), Space: 44.5 MB (82.00%) - LeetHub

---
+++ LeetCode/Medium/0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        if (s.length() < p.length()) return new ArrayList<>();
+        
+        char[] ref = new char[26];
+        for (int i = 0; i < p.length(); i++)
+            ref[p.charAt(i) - 'a']++;
+        
+        List<Integer> result = new ArrayList<>();
+        char[] window = new char[26];
+        for (int i = 0; i < s.length(); i++) {
+            window[s.charAt(i) - 'a']++;
+            if (i >= p.length()) window[s.charAt(i-p.length()) - 'a']--;
+            if (Arrays.compare(ref, window) == 0)
+                result.add(i - p.length() + 1);
+        }
+        return result;
+    }
+}
