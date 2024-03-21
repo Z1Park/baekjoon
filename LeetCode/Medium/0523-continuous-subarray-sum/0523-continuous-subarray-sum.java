26f81d3090f3f54056a549bc72f26b10505d1dbe
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-21 18:18:22 +0900

    Time: 21 ms (92.85%), Space: 63.6 MB (5.51%) - LeetHub

--- LeetCode/Medium/0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ LeetCode/Medium/0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -2,14 +2,12 @@ class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         int sum = 0;
+        map.put(0, -1);
         for (int i = 0; i < nums.length; i++) {
             sum = (sum + nums[i]) % k;
             if (sum == 0 && i > 0) return true;
-            if (map.containsKey(sum)) {
-                if (i - map.get(sum) > 1)
-                    return true;
-            }
-            else map.put(sum, i);
+            if (!map.containsKey(sum)) map.put(sum, i);
+            else if (i - map.get(sum) > 1) return true;
         }
         return false;
     }
