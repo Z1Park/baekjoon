8a24e9cd88c7d55fc952ad8fcdf1c613680d8199
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-21 14:36:02 +0900

    Time: 3 ms (84.32%), Space: 41.1 MB (98.84%) - LeetHub

--- LeetCode/Medium/0139-word-break/0139-word-break.java
+++ LeetCode/Medium/0139-word-break/0139-word-break.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> dict = new HashSet<>(wordDict);
         boolean[] possible = new boolean[s.length()+1];
         possible[0] = true;
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i+1; j <= s.length(); j++) {
-                if (possible[i] && dict.contains(s.substring(i, j)))
-                    possible[j] = true;
+            if (!possible[i]) continue;
+            for (String word : wordDict) {
+                if (word.charAt(0) != s.charAt(i) || i+word.length() > s.length())
+                    continue;
+                if (word.equals(s.substring(i, i+word.length())))
+                    possible[i+word.length()] = true;
             }
         }
         return possible[s.length()];
