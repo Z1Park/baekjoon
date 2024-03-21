069d0f550717d96d02c99ffa9643bbdc39d856db
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-21 19:33:01 +0900

    Time: 24 ms (78.98%), Space: 45.9 MB (86.32%) - LeetHub

--- LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -5,7 +5,8 @@ class Solution {
         map.put(0, 1);
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            count += map.getOrDefault(sum-k, 0);
+            if (map.containsKey(sum-k))
+                count += map.get(sum-k);
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;
