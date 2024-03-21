0efae80cd828be60c642843d1cdfe6e09c712156
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-21 20:50:23 +0900

    Time: 0 ms (100.00%), Space: 44.7 MB (56.31%) - LeetHub

--- LeetCode/Medium/0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ LeetCode/Medium/0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,20 +1,19 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int[][] dp = new int[nums.length][2];
-        int maxValue = nums[0];
-        if (nums[0] > 0) dp[0][0] = nums[0];
-        else dp[0][1] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                dp[i][0] = Math.max(nums[i], dp[i-1][0] * nums[i]);
-                dp[i][1] = Math.min(nums[i], dp[i-1][1] * nums[i]);
-            }
-            else  {
-                dp[i][0] = Math.max(nums[i], dp[i-1][1] * nums[i]);
-                dp[i][1] = Math.min(nums[i], dp[i-1][0] * nums[i]);
-            }
-            if (maxValue < dp[i][0]) maxValue = dp[i][0];
+        int max = Integer.MIN_VALUE, product = 1;
+        for (int i = 0; i < nums.length; i++) {
+            product *= nums[i];
+            if (max < product) max = product;
+            if (max < nums[i]) max = nums[i];
+            if (product == 0) product = 1;
         }
-        return maxValue;
+        product = 1;
+        for (int i = nums.length-1; i >= 0; i--) {
+            product *= nums[i];
+            if (max < product) max = product;
+            if (max < nums[i]) max = nums[i];
+            if (product == 0) product = 1;
+        }
+        return max;
     }
 }
