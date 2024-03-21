160516dd13e0fcb26297995dc7d36cc94cd607b0
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-21 15:50:09 +0900

    Time: 5 ms (68.89%), Space: 44.2 MB (89.46%) - LeetHub

--- LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
+++ LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
@@ -1,35 +1,29 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer>[] graph = new ArrayList[numCourses];
-        for (int i = 0; i <  numCourses; i++) {
-            graph[i] = new ArrayList<>();
-            graph[i].add(0);
-        }
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] counts = new int[numCourses];
+        for (int i = 0; i <  numCourses; i++)
+            graph.add(new ArrayList<>());
         for (int[] e : prerequisites) {
-            graph[e[0]].add(e[1]);
-            graph[e[1]].set(0, graph[e[1]].get(0)+1);
+            graph.get(e[0]).add(e[1]);
+            counts[e[1]]++;
         }
         
         Queue<Integer> que = new LinkedList<>();
-        for (int i = 0; i < graph.length; i++) {
-            if (graph[i].get(0) == 0)
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] == 0)
                 que.add(i);
         }
-        if (que.isEmpty()) return false;
         while (!que.isEmpty()) {
             int course = que.poll();
-            for (int i = graph[course].size()-1; i > 0; i--) {
-                int next = graph[course].get(i);
-                graph[course].remove(i);
-                graph[next].set(0, graph[next].get(0)-1);
-                if (graph[next].get(0) == 0)
+            for (int next : graph.get(course)) {
+                if (--counts[next] == 0)
                     que.add(next);
             }
         }
         if (!que.isEmpty()) return false;
-        for (int i = 0; i < graph.length; i++) {
-            if (graph[i].size() != 1) return false;
-        }
+        for (int i = 0; i < counts.length; i++)
+            if (counts[i] != 0) return false;
         return true;
     }
 }
