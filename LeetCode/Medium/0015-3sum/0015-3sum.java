af2e2d181c1d668372c0500aa7aaaa930765dd24
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-22 12:28:08 +0900

    Time: 31 ms (66.10%), Space: 51 MB (91.85%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.java
+++ LeetCode/Medium/0015-3sum/0015-3sum.java
@@ -1,30 +1,28 @@
 class Solution {
-    
-    private int binarySearch(int[] nums, int start, int end, int target) {
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (nums[mid] < target) start = mid+1;
-            else end = mid-1;
-        }
-        return start;
-    }
-    
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        Set<List<Integer>> result = new HashSet<>();
-        int size = nums.length - 2;
+        List<List<Integer>> result = new ArrayList<>();
+        
+        int size = nums.length- 2;
         for (int i = 0; i < size; i++) {
             if (i > 0 && nums[i] == nums[i-1]) continue;
-            int sum = 0, limit = nums.length;
-            for (int j = i+1; j < limit; j++) {
-                sum = nums[i] + nums[j];
-                int idx = binarySearch(nums, j+1, limit-1, -sum);
-                if (j < idx && idx < limit && sum + nums[idx] == 0) {
-                    result.add(Arrays.asList(nums[i], nums[j], nums[idx]));
-                    limit = idx;
+            
+            int l = i+1, r = nums.length-1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l+1])
+                        l++;
+                    while (l < r && nums[r] == nums[r-1])
+                        r--;
+                    l++;
+                    r--;
                 }
+                else if (sum > 0) r--;
+                else l++;
             }
         }
-        return new ArrayList<>(result);
+        return result;
     }
 }
