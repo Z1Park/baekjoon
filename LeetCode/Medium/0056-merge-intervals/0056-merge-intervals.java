138eb38974ffabcf037401f8387098c5cfa654b4
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-27 12:24:41 +0900

    Time: 8 ms (88.02%), Space: 46.7 MB (15.99%) - LeetHub

--- LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.java
+++ LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        
         List<int[]> result = new ArrayList<>();
         int start = intervals[0][0], end = intervals[0][1];
-        for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] > end) {
+        for (int[] interval : intervals) {
+            if (end < interval[0]) {
                 result.add(new int[]{start, end});
-                start = intervals[i][0];
-                end = intervals[i][1];
+                start = interval[0];
+                end = interval[1];
             }
-            else if (end < intervals[i][1]) end = intervals[i][1];
+            else if (end < interval[1]) end = interval[1];
         }
         result.add(new int[]{start, end});
         return result.toArray(int[][]::new);
