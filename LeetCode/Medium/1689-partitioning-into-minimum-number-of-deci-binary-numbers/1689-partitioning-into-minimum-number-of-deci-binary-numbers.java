b82598133a7db57ebc9a9fcb31dcd028ae7ba2bf
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-28 12:24:45 +0900

    Time: 4 ms (91.81%), Space: 45 MB (75.74%) - LeetHub

--- LeetCode/Medium/1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ LeetCode/Medium/1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -2,7 +2,7 @@ class Solution {
     public int minPartitions(String n) {
         int result = 0;
         for (char c : n.toCharArray())
-            result = Math.max(result, c - '0');
-        return result;
+            if (result < c) result = c;
+        return result - '0';
     }
 }
