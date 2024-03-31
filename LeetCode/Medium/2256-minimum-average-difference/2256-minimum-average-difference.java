32a4412da0c0a8f025de602e1f962bc4a07881ae
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-03-31 14:58:40 +0900

    Time: 4 ms (96.70%), Space: 62.2 MB (18.02%) - LeetHub

--- LeetCode/Medium/2256-minimum-average-difference/2256-minimum-average-difference.java
+++ LeetCode/Medium/2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -5,11 +5,12 @@ class Solution {
         for (int i = 0; i < len; i++)
             rsum += nums[i];
         
-        int idx = -1, minValue = Integer.MAX_VALUE;
+        int idx = -1;
+        long minValue = Integer.MAX_VALUE;
         for (int i = 1; i <= len; i++) {
             lsum += nums[i-1];
             rsum -= nums[i-1];
-            int diff = Math.abs((int)((lsum / i) - (i == len ? 0 : rsum / (len - i))));
+            long diff = Math.abs(((lsum / i) - (i == len ? 0 : rsum / (len - i))));
             if (minValue > diff) {
                 minValue = diff;
                 idx = i-1;
