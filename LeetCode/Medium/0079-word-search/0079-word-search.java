3c82a08169149a7c73e518e60b6ee307e9220b75
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-04-03 12:34:20 +0900

    Time: 138 ms (70.43%), Space: 41.6 MB (49.08%) - LeetHub

--- LeetCode/Medium/0079-word-search/0079-word-search.java
+++ LeetCode/Medium/0079-word-search/0079-word-search.java
@@ -1,39 +1,39 @@
 class Solution {
     
-    int R, C;
-    boolean[][] visit;
-    int[] dr = {0, 1, 0, -1};
-    int[] dc = {1, 0, -1, 0};
+    private int N, M;
+    private int[] dr = {0, 1, 0, -1};
+    private int[] dc = {1, 0, -1, 0};
+    private boolean[][] used;
     
-    private boolean dfs(char[][] board, String word, int r, int c, int depth) {
+    private boolean dfs(char[][] board, String word, int r, int c, int idx) {
+        if (idx >= word.length()) return true;
+        used[r][c] = true;
+        
+        char cc = word.charAt(idx);
         for (int i = 0; i < 4; i++) {
             int nr = r + dr[i], nc = c + dc[i];
-            if (nr < 0 || R <= nr || nc < 0 || C <= nc)
+            if (nr < 0 || N <= nr || nc < 0 || M <= nc)
                 continue;
-            if (word.charAt(depth) == board[nr][nc] && !visit[nr][nc]) {
-                visit[nr][nc] = true;
-                if (depth + 1 == word.length() || 
-                    dfs(board, word, nr, nc, depth + 1))
-                    return true;
-                visit[nr][nc] = false;
+            
+            if (board[nr][nc] == cc && !used[nr][nc] 
+                && dfs(board, word, nr, nc, idx+1)) {
+                return true;
             }
         }
+        used[r][c] = false;
         return false;
     }
     
     public boolean exist(char[][] board, String word) {
-        R = board.length;
-        C = board[0].length;
-        visit = new boolean[R][C];
+        N = board.length;
+        M = board[0].length;
+        used = new boolean[N][M];
+        
         char c = word.charAt(0);
-        for (int i = 0 ; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                if (board[i][j] != c) continue;
-                if (word.length() == 1) return true;
-                visit[i][j] =  true;
-                if (dfs(board, word, i, j, 1))
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (board[i][j] == c && dfs(board, word, i, j, 1))
                     return true;
-                visit[i][j] = false;
             }
         }
         return false;
