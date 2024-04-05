4a985f6cb12a9a607e05b8cd42d79938a5ae0b05
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-04-05 16:12:31 +0900

    Time: 7 ms (100.00%), Space: 45.3 MB (35.50%) - LeetHub

--- LeetCode/Easy/1603-design-parking-system/1603-design-parking-system.java
+++ LeetCode/Easy/1603-design-parking-system/1603-design-parking-system.java
@@ -3,32 +3,19 @@ class ParkingSystem {
     private int big;
     private int medium;
     private int small;
-    private Map<Integer, Integer> parkingMap = new HashMap<>();
 
     public ParkingSystem(int big, int medium, int small) {
         this.big = big;
         this.medium = medium;
         this.small = small;
-        
-        parkingMap.put(1, 0);
-        parkingMap.put(2, 0);
-        parkingMap.put(3, 0);
-    }
-    
-    private int getLimit(int carType) {
-        return switch (carType) {
-            case 1 -> big;
-            case 2 -> medium;
-            case 3 -> small;
-            default -> -1;
-        };
     }
     
     public boolean addCar(int carType) {
-        int parkingCount = parkingMap.get(carType);
-        if (parkingCount == getLimit(carType))
-            return false;
-        parkingMap.put(carType, parkingCount + 1);
+        switch (carType) {
+            case 1 -> {if (big-- <= 0) return false;}
+            case 2 -> {if (medium-- <= 0) return false;}
+            case 3 -> {if (small-- <= 0) return false;}
+        }
         return true;
     }
 }
