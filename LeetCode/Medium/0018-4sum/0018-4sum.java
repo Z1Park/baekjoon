409c269f575322fcb6428848ce24ea5b75be92d3
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-04-07 10:28:45 +0900

    Time: 25 ms (27.38%), Space: 45.5 MB (8.01%) - LeetHub

--- LeetCode/Medium/0018-4sum/0018-4sum.java
+++ LeetCode/Medium/0018-4sum/0018-4sum.java
@@ -5,14 +5,17 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         int len = nums.length - 3;
-        for (int i = 0; i < len; i++)
+        for (int i = 0; i < len; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
             threeSum(nums, i, target - nums[i]);
+        }
         return new ArrayList<>(result);
     }
     
     private void threeSum(int[] nums, int idx, int target) {
-        int len = nums.length - 2;
-        for (int i = idx+1; i < len; i++) {
+        int len = nums.length - 2, start = idx+1;
+        for (int i = start; i < len; i++) {
+            // if (nums[i] != start && nums[i] == nums[i-1]) continue;
             int l = i+1, r = nums.length-1;
             while (l < r) {
                 long sum = (long)nums[i] + nums[l] + nums[r];
