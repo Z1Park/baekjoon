db882336e591b51270154b2585c0e3a2bff4adeb
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-04-07 10:23:21 +0900

    Time: 276 ms (6.95%), Space: 45.6 MB (8.01%) - LeetHub

---
+++ LeetCode/Medium/0018-4sum/0018-4sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    private Set<List<Integer>> result = new HashSet<>();
+    
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        int len = nums.length - 3;
+        for (int i = 0; i < len; i++)
+            threeSum(nums, i, target - nums[i]);
+        return new ArrayList<>(result);
+    }
+    
+    private void threeSum(int[] nums, int idx, int target) {
+        int len = nums.length - 2;
+        for (int i = idx+1; i < len; i++) {
+            int l = i+1, r = nums.length-1;
+            while (l < r) {
+                long sum = (long)nums[i] + nums[l] + nums[r];
+                if (sum == target)
+                    result.add(Arrays.asList(nums[idx], nums[i], nums[l], nums[r]));
+                
+                if (sum < target) l++;
+                else r--;
+            }
+        }
+    }
+}
