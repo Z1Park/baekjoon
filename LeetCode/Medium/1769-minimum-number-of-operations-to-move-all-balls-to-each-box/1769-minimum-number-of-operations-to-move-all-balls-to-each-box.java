ed3b971c41be27cf590d145ef16d6cb00af10c03
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-04-13 11:04:31 +0900

    Time: 3 ms (96.56%), Space: 44.5 MB (76.67%) - LeetHub

--- LeetCode/Medium/1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ LeetCode/Medium/1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,14 +1,27 @@
 class Solution {
     public int[] minOperations(String boxes) {
         int n = boxes.length();
+        
         int[] result = new int[n];
+        int leftCount = 0, rightCount = 0;
+        int leftSum = 0, rightSum = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                if (boxes.charAt(j) == '1') 
-                    result[i] += Math.abs(i - j);
+            if (boxes.charAt(i) == '1') {
+                rightCount++;
+                rightSum += i;
             }
         }
+        
+        for (int i = 0; i < n; i++) {
+            result[i] = leftSum + rightSum;
+            if (boxes.charAt(i) == '1') {
+                leftCount++;
+                rightCount--;
+            }
+            rightSum -= rightCount;
+            leftSum += leftCount;
+        }
+        
         return result;
     }
 }
