4cd52e9f42558d3fb6806e5e3ab5e7749d3509eb
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-04-18 17:26:33 +0900

    Time: 2 ms (98.81%), Space: 57 MB (45.76%) - LeetHub

--- LeetCode/Medium/0134-gas-station/0134-gas-station.java
+++ LeetCode/Medium/0134-gas-station/0134-gas-station.java
@@ -1,20 +1,19 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int idx = 0;
-        while (true) {
-            int sum = 0;
-            boolean flag = true;
-            for (int i = 0; i < gas.length; i++) {
-                int index = (i + idx) % gas.length;
-                sum += gas[index] - cost[index];
-                if (sum < 0) {
-                    idx = i + idx +1;
-                    flag = false;
-                    break;
-                }
+        int sum = 0;
+        for (int i = 0; i < gas.length; i++)
+            sum += gas[i] - cost[i];
+        if (sum < 0) return -1;
+        
+        sum = 0;
+        int res = 0;
+        for (int i = 0; i < gas.length; i++) {
+            sum += gas[i] - cost[i];
+            if (sum < 0) {
+                sum = 0;
+                res = i+1;
             }
-            if (idx >= gas.length) return -1;
-            if (flag) return idx;
         }
+        return res;
     }
 }
