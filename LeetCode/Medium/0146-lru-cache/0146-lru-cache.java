d36a275697e1de063fb6c069ae8ecaceb51fd582
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-04-20 15:35:37 +0900

    Time: 35 ms (98.52%), Space: 106.9 MB (97.79%) - LeetHub

--- LeetCode/Medium/0146-lru-cache/0146-lru-cache.java
+++ LeetCode/Medium/0146-lru-cache/0146-lru-cache.java
@@ -1,47 +1,70 @@
 class LRUCache {
     
+    private class Node {
+        
+        int key;
+        int value;
+        Node prev;
+        Node next;
+        
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
     private int capacity;
-    private int time = 0, minTime = 0;
-    private Map<Integer, List<Integer>> cache = new HashMap<>();
-    private Map<Integer, Integer> que = new HashMap<>();
+    private int size = 0;
+    private Node head;
+    private Node tail;
+    private Node[] map = new Node[10_001];
+    
+    
+    private void addNode(Node reference, Node node) {
+        node.next = reference.next;
+        node.prev = reference;
+        node.next.prev = node;
+        reference.next = node;
+    }
+    
+    private void deleteNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head.next = tail;
+        tail.prev = head;
     }
     
     public int get(int key) {
-        if (cache.containsKey(key)) {
-            List<Integer> cc = cache.get(key);
-            if (minTime == cc.get(1))
-                minTime++;
-            que.remove(cc.get(1));
-            cc.set(1, ++time);
-            que.put(time, key);
-            return cc.get(0);
-        }
-        return -1;
+        if (map[key] == null) return -1;
+        Node node = map[key];
+        deleteNode(node);
+        addNode(head, node);
+        return node.value;
     }
     
+    
     public void put(int key, int value) {
-        time++;
-        if (cache.containsKey(key)) {
-            List<Integer> cc = cache.get(key);
-            if (minTime == cc.get(1))
-                minTime++;
-            que.remove(cc.get(1));
-            cc.set(0, value);
-            cc.set(1, time);
-            que.put(time, key);
+        if (map[key] == null) {
+            map[key] = new Node(key, value);
+            addNode(head, map[key]);
+            if (size == capacity) {
+                map[tail.prev.key] = null;
+                deleteNode(tail.prev);
+            }
+            else size++;
         }
         else {
-            cache.put(key, Arrays.asList(value, time));
-            que.put(time, key);
-            if (cache.size() > capacity) {
-                while (!que.containsKey(minTime))
-                    minTime++;
-                cache.remove(que.get(minTime));
-                que.remove(minTime);
-            }
+            Node node = map[key];
+            node.value = value;
+            deleteNode(node);
+            addNode(head, node);
         }
     }
 }
