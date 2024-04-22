6646c22123c5af32837e0f2c8acd04d2e8b63d84
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-04-22 13:50:55 +0900

    Time: 2 ms (70.48%), Space: 44.1 MB (69.39%) - LeetHub

--- LeetCode/Medium/1094-car-pooling/1094-car-pooling.java
+++ LeetCode/Medium/1094-car-pooling/1094-car-pooling.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        Arrays.sort(trips, (a, b) -> a[1] - b[1]);
-        PriorityQueue<int[]> que = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        int count = 0;
+        int[] prefix = new int[1001];
         for (int[] trip : trips) {
-            while (!que.isEmpty() && que.peek()[0] <= trip[1])
-                count -= que.poll()[1];
-            if (count + trip[0] > capacity)
+            prefix[trip[1]] += trip[0];
+            prefix[trip[2]] -= trip[0];
+        }
+        
+        if (prefix[0] > capacity) return false;
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] += prefix[i-1];
+            if (prefix[i] > capacity)
                 return false;
-            count += trip[0];
-            que.add(new int[]{trip[2], trip[0]});
         }
         return true;
     }
