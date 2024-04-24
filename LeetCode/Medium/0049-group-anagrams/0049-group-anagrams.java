7274481464645ef4df5a89256787ad137fced6a4
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-04-24 15:08:57 +0900

    Time: 8 ms (38.38%), Space: 47.6 MB (74.14%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
@@ -5,8 +5,9 @@ class Solution {
             char[] alp = new char[26];
             for (int i = 0; i < str.length(); i++)
                 alp[str.charAt(i) - 'a']++;
-            map.computeIfAbsent(String.valueOf(alp), k -> new ArrayList<>())
-                .add(str);
+            String key = String.valueOf(alp);
+            if (!map.containsKey(key)) map.put(key, new ArrayList<>());
+            map.get(key).add(str);
         }
         return map.values().stream().toList();
     }
