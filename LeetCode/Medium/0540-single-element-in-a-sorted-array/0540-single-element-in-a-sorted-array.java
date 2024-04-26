497278b3b9ba19f23ad1feff3fb5e9637e1ae160
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-04-26 12:54:57 +0900

    Time: 0 ms (100.00%), Space: 50 MB (83.73%) - LeetHub

--- LeetCode/Medium/0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ LeetCode/Medium/0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,20 +1,17 @@
 class Solution {
     
-    private int checkAllFilled(int[] nums, int idx) {
-        if (idx > 0 && nums[idx-1] == nums[idx])
-            idx--;
-        else if (idx >= nums.length - 1 || nums[idx] != nums[idx+1])
-            return 0;
-        return (idx % 2 == 0 ? 1 : -1);
+    private boolean checkAllFilled(int[] nums, int idx) {
+        if (idx > 0 && nums[idx-1] == nums[idx]) idx--;
+        return idx % 2 == 0;
     }
     
     public int singleNonDuplicate(int[] nums) {
         int start = 0, end = nums.length-1;
         while (start <= end) {
             int mid = (start + end) / 2;
-            if (checkAllFilled(nums, mid) > 0) start = mid + 1;
+            if (checkAllFilled(nums, mid)) start = mid + 1;
             else end = mid - 1;
         }
-        return nums[start];
+        return nums[end];
     }
 }
