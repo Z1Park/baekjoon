7dfb8bde68a103ebacd0047676e059b32acec157
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-04-29 14:43:12 +0900

    Time: 8 ms (15.74%), Space: 44.4 MB (22.60%) - LeetHub

--- LeetCode/Medium/0120-triangle/0120-triangle.java
+++ LeetCode/Medium/0120-triangle/0120-triangle.java
@@ -1,20 +1,21 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] dp = new int[n][n];
         
-        dp[0][0] = triangle.get(0).get(0);
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (j == 0) dp[i][j] = dp[i-1][j];
-                else if (j == i) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j]);
-                dp[i][j] += triangle.get(i).get(j);
-            }
+            triangle.get(i).set(0, triangle.get(i).get(0) + 
+                                triangle.get(i-1).get(0));
+            for (int j = 1; j < i; j++)
+                triangle.get(i).set(j, triangle.get(i).get(j) + 
+                                    Math.min(triangle.get(i-1).get(j-1), 
+                                            triangle.get(i-1).get(j)));
+            triangle.get(i).set(i, triangle.get(i).get(i) + 
+                                triangle.get(i-1).get(i-1));
         }
+        
         int minValue = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++)
-            minValue = Math.min(dp[n-1][i], minValue);
+            minValue = Math.min(triangle.get(n-1).get(i), minValue);
         return minValue;
     }
 }
