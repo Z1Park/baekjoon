a191c501878b5fa03be6bce3f7526f714cdf7783
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-04-30 17:14:41 +0900

    Time: 1 ms (86.99%), Space: 45.6 MB (17.84%) - LeetHub

--- LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.java
+++ LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.java
@@ -1,26 +1,13 @@
 class Solution {
-    
-    private boolean isOnLeft(int[] nums, int idx) {
-        if (idx == 0) return nums[idx] != nums[idx+1];
-        if (idx == nums.length - 1) return true;
-        
-        int left;
-        if (nums[idx] == nums[idx-1])
-            left = idx - (nums[idx] == nums[idx+1] ? 1 : 2);
-        else if (nums[idx] == nums[idx+1]) left = idx;
-        else return true;
-        return left % 3 != 0;
-    }
-    
     public int singleNumber(int[] nums) {
-        if (nums.length == 1) return nums[0];
-        Arrays.sort(nums);
-        int start = 0, end = nums.length-1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (isOnLeft(nums, mid)) end = mid - 1;
-            else start = mid + 1;
+        int ans = 0;
+        for (int i = 0; i <= 32; i++) {
+            int sum = 0;
+            for (int num : nums)
+                sum += (num >> i) & 1;
+            sum %= 3;
+            ans |= sum << i;
         }
-        return nums[start];
+        return ans;
     }
 }
