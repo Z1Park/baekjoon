b13a0447136064927484502bafd23df86129c12f
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-05-04 19:22:19 +0900

    Time: 9 ms (5.10%), Space: 44.8 MB (68.59%) - LeetHub

--- LeetCode/Medium/0529-minesweeper/0529-minesweeper.java
+++ LeetCode/Medium/0529-minesweeper/0529-minesweeper.java
@@ -1,20 +1,22 @@
 class Solution {
     
     private int N, M;
-    private int[] dr = {0, 1, 0, -1, 1, 1, -1, -1};
-    private int[] dc = {1, 0, -1, 0, 1, -1, 1, -1};
     
     private int[][] getCountBoard(char[][] board) {
+        int[][] prefix = new int[N+1][M+1];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                prefix[i+1][j+1] = prefix[i+1][j] + prefix[i][j+1] - prefix[i][j] + (board[i][j] == 'M' ? 1 : 0);
+            }
+        }
+        
         int[][] countBoard = new int[N][M];
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < M; c++) {
-                if (board[r][c] != 'M') continue;
-                for (int i = 0; i < 8; i++) {
-                    int nr = r + dr[i], nc = c + dc[i];
-                    if (nr < 0 || nr >= N || nc < 0 || nc >= M)
-                        continue;
-                    countBoard[nr][nc]++;
-                }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (board[i-1][j-1] == 'M') continue;
+                int top = Math.max(0, i-2), left = Math.max(0, j-2);
+                int bottom = Math.min(N, i+1), right = Math.min(M, j+1);
+                countBoard[i-1][j-1] = prefix[bottom][right] - prefix[bottom][left] - prefix[top][right] + prefix[top][left];
             }
         }
         return countBoard;
@@ -28,6 +30,8 @@ class Solution {
         
         N = board.length;
         M = board[0].length;
+        int[] dr = {0, 1, 0, -1, 1, 1, -1, -1};
+        int[] dc = {1, 0, -1, 0, 1, -1, 1, -1};
         int[][] countBoard = getCountBoard(board);
         
         Queue<int[]> que = new LinkedList<>();
