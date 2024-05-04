4b9556845d5cc8bde96a7416d898aa6d7e8a1fb7
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-05-04 19:17:53 +0900

    Time: 8 ms (5.10%), Space: 44.5 MB (92.19%) - LeetHub

--- LeetCode/Medium/0529-minesweeper/0529-minesweeper.java
+++ LeetCode/Medium/0529-minesweeper/0529-minesweeper.java
@@ -1,43 +1,61 @@
 class Solution {
+    
+    private int N, M;
+    
+    private int[][] getCountBoard(char[][] board) {
+        int[][] prefix = new int[N+1][M+1];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                prefix[i+1][j+1] = prefix[i+1][j] + prefix[i][j+1] - prefix[i][j] + (board[i][j] == 'M' ? 1 : 0);
+            }
+        }
+        
+        int[][] countBoard = new int[N][M];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (board[i-1][j-1] == 'M') continue;
+                int top = Math.max(0, i-2), left = Math.max(0, j-2);
+                int bottom = Math.min(N, i+1), right = Math.min(M, j+1);
+                countBoard[i-1][j-1] = prefix[bottom][right] - prefix[bottom][left] - prefix[top][right] + prefix[top][left];
+            }
+        }
+        return countBoard;
+    }
+    
     public char[][] updateBoard(char[][] board, int[] click) {
-        int N = board.length, M = board[0].length;
+        if (board[click[0]][click[1]] == 'M') {
+            board[click[0]][click[1]] = 'X';
+            return board;
+        }
+        
+        N = board.length;
+        M = board[0].length;
         int[] dr = {0, 1, 0, -1, 1, 1, -1, -1};
         int[] dc = {1, 0, -1, 0, 1, -1, 1, -1};
+        int[][] countBoard = getCountBoard(board);
         boolean[][] visit = new boolean[N][M];
         
         Queue<int[]> que = new LinkedList<>();
         que.add(click);
         visit[click[0]][click[1]] = true;
+        board[click[0]][click[1]] = (countBoard[click[0]][click[1]] == 0 ? 'B' : (char)(countBoard[click[0]][click[1]] + '0'));
         while (!que.isEmpty()) {
             int[] tmp = que.poll();
             int r = tmp[0], c = tmp[1];
-            
-            if (board[r][c] == 'M') {
-                board[r][c] = 'X';
-                continue;
-            }
-            
-            int mineCount = 0;
-            for (int i = 0; i < 8; i++) {
-                int nr = r + dr[i], nc = c + dc[i];
-                if (nr < 0 || nr >= N || nc < 0 || nc >= M)
-                    continue;
-                if (board[nr][nc] == 'M')
-                    mineCount++;
-            }
-            if (mineCount == 0) {
-                board[r][c] = 'B';
+            if (countBoard[r][c] == 0) {
                 for (int i = 0; i < 8; i++) {
                     int nr = r + dr[i], nc = c + dc[i];
                     if (nr < 0 || nr >= N || nc < 0 || nc >= M)
                         continue;
-                    if (!visit[nr][nc] && board[nr][nc] == 'E') {
-                        visit[nr][nc] = true;
-                        que.add(new int[]{nr, nc});
+                    if (board[nr][nc] == 'E') {
+                        if (countBoard[nr][nc] == 0) {
+                            board[nr][nc] = 'B';
+                            que.add(new int[]{nr, nc});
+                        }
+                        else board[nr][nc] = (char)(countBoard[nr][nc] + '0');
                     }
                 }
             }
-            else board[r][c] = (char)(mineCount + '0');
         }
         return board;
     }
