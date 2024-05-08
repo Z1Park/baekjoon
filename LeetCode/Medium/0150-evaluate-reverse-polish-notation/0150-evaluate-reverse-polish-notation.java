a35b572d04f8be1d729b32c333139cfd09302df0
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-05-08 14:56:30 +0900

    Time: 5 ms (96.96%), Space: 44.6 MB (47.71%) - LeetHub

--- LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -6,13 +6,11 @@ class Solution {
             case '-' -> num1 - num2;
             case '*' -> num1 * num2;
             case '/' -> num1 / num2;
-            default -> -1;
+            default -> 0;
         };
     }
     
     public int evalRPN(String[] tokens) {
-        if (tokens.length == 1) return Integer.valueOf(tokens[0]);
-        
         Deque<Integer> stk = new ArrayDeque<>();
         for (String token : tokens) {
             if (Character.isDigit(token.charAt(token.length()-1)))
