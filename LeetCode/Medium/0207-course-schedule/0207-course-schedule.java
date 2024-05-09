929c3e4f099e78e47de97dc9c667d480136245ad
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-05-09 12:42:45 +0900

    Time: 4 ms (80.32%), Space: 44.3 MB (81.90%) - LeetHub

--- LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
+++ LeetCode/Medium/0207-course-schedule/0207-course-schedule.java
@@ -1,29 +1,37 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> graph = new ArrayList<>();
         int[] counts = new int[numCourses];
-        for (int i = 0; i <  numCourses; i++)
-            graph.add(new ArrayList<>());
-        for (int[] e : prerequisites) {
-            graph.get(e[0]).add(e[1]);
-            counts[e[1]]++;
+        List<Integer>[] graph = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++)
+            graph[i] = new ArrayList<>();
+        
+        for (int[] pr : prerequisites) {
+            counts[pr[0]]++;
+            graph[pr[1]].add(pr[0]);
         }
         
+        boolean[] visit = new boolean[numCourses];
         Queue<Integer> que = new LinkedList<>();
-        for (int i = 0; i < counts.length; i++) {
-            if (counts[i] == 0)
+        for (int i = 0; i < numCourses; i++) {
+            if (counts[i] == 0) {
+                visit[i] = true;
                 que.add(i);
-        }
-        while (!que.isEmpty()) {
-            int course = que.poll();
-            for (int next : graph.get(course)) {
-                if (--counts[next] == 0)
-                    que.add(next);
             }
         }
-        if (!que.isEmpty()) return false;
-        for (int i = 0; i < counts.length; i++)
+        while (!que.isEmpty()) {
+            int curr = que.poll();
+            for (int next : graph[curr]) {
+                counts[next]--;
+                if (counts[next] == 0 && !visit[next]) {
+                    que.add(next);
+                    visit[next] = true;
+                }
+            }
+        }
+        
+        for (int i = 0; i < numCourses; i++) {
             if (counts[i] != 0) return false;
+        }
         return true;
     }
 }
