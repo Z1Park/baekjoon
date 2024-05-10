a2bc233cb91c35fef78f6e1630b5784fff3613a8
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-05-10 20:51:36 +0900

    Time: 63 ms (96.65%), Space: 58.7 MB (92.99%) - LeetHub

--- LeetCode/Medium/2895-minimum-processing-time/2895-minimum-processing-time.java
+++ LeetCode/Medium/2895-minimum-processing-time/2895-minimum-processing-time.java
@@ -1,11 +1,11 @@
 class Solution {
     public int minProcessingTime(List<Integer> processorTime, List<Integer> tasks) {
         Collections.sort(processorTime);
-        Collections.sort(tasks, Collections.reverseOrder());
+        Collections.sort(tasks);
         
         int len = processorTime.size(), result = Integer.MIN_VALUE;
-        for (int i = 0; i < len; i++)
-            result = Math.max(result, processorTime.get(i) + tasks.get(i * 4));
+        for (int i = 0, j = tasks.size()-1; i < len; i++, j -= 4)
+            result = Math.max(result, processorTime.get(i) + tasks.get(j));
         return result;
     }
 }
