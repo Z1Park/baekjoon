6567e83e8db4098711380959d95ab2cbe46495e6
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-05-16 16:05:23 +0900

    swea 삭제

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,30 +1,26 @@
 class Solution {
     
-    private int getRotatedIndex(int[] nums, int s, int e) {
-        if (nums[s] < nums[e]) return e+1;
-        if (s == e) return e;
-        if (e - s == 1 && nums[s] > nums[e]) return e;
-        int mid = (s + e) / 2;
-        if (nums[s] > nums[mid]) return getRotatedIndex(nums, s, mid);
-        else return getRotatedIndex(nums, mid, e);
+    private int findPivot(int[] nums, int start, int end) {
+        if (nums[start] <= nums[end]) return start;
+        int mid = (start + end) / 2;
+        if (nums[start] > nums[mid]) return findPivot(nums, start, mid);
+        return findPivot(nums, mid+1, end);
     }
     
-    private int binarySearch(int[] nums, int target, int s, int e) {
-        while (s <= e) {
-            int mid = (s + e) / 2;
-            if (nums[mid] < target) s = mid + 1;
-            else e = mid - 1;
+    private int findValue(int[] nums, int start, int end, int target) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] < target) start = mid + 1;
+            else end = mid - 1;
         }
-        return s;
+        if (start >= nums.length || nums[start] != target)
+            return -1;
+        return start;
     }
     
     public int search(int[] nums, int target) {
-        int rot = getRotatedIndex(nums, 0, nums.length-1);
-        int idx = binarySearch(nums, target, 0, rot-1);
-        if (idx >= rot || nums[idx] != target)
-            idx = binarySearch(nums, target, rot, nums.length-1);
-        if (idx >= nums.length || nums[idx] != target)
-            idx = -1;
-        return idx;
+        int end = nums.length - 1, pivot = findPivot(nums, 0, end);
+        if (nums[end] >= target) return findValue(nums, pivot, end, target);
+        return findValue(nums, 0, pivot-1, target);
     }
 }
