f78701a5b15343f5b18809f6cbf2cff541305eaa
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-05-20 12:20:55 +0900

    Time: 0 ms (100%), Space: 42.8 MB (92.32%) - LeetHub

--- LeetCode/Medium/0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ LeetCode/Medium/0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,14 +14,13 @@
  * }
  */
 class Solution {
-    
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return isValidBST(root, Long.MAX_VALUE, Long.MIN_VALUE);
     }
-    
-    private boolean isValidBST(TreeNode root, long min, long max) {
+
+    private boolean isValidBST(TreeNode root, long max, long min) {
         if (root == null) return true;
-        if (root.val >= max || root.val <= min) return false;
-        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+        if (root.val <= min || root.val >= max) return false;
+        return isValidBST(root.right, max, root.val) && isValidBST(root.left, root.val, min);
     }
 }
