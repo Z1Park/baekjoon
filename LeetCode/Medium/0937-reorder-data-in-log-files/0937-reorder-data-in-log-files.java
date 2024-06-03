1aa6f171fd9fd6a982205bb6347ae97c3bc8cbb0
Jiwon Park <82518170+Z1Park@users.noreply.github.com>
2024-06-03 16:22:15 +0900

    Time: 4 ms (74.55%), Space: 44.6 MB (71.77%) - LeetHub

--- LeetCode/Medium/0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
+++ LeetCode/Medium/0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
@@ -7,31 +7,21 @@ class Solution {
             if (Character.isDigit(split[1].charAt(0))) digitList.add(log);
             else letterList.add(split);
         }
-        Collections.sort(letterList, new Comparator<String[]>(){
-            @Override
-            public int compare(String[] o1, String[] o2) {
-                int len = Math.min(o1.length, o2.length);
-                for (int i = 1; i < len; i++) {
-                    int comparsion = o1[i].compareTo(o2[i]);
-                    if (comparsion != 0)
-                        return comparsion;
-                }
-                int comparsion = Integer.compare(o1.length, o2.length);
-                if (comparsion != 0) return comparsion;
-                return o1[0].compareTo(o2[0]);
+        Collections.sort(letterList, (o1, o2) -> {
+            int len = Math.min(o1.length, o2.length);
+            for (int i = 1; i < len; i++) {
+                int comparsion = o1[i].compareTo(o2[i]);
+                if (comparsion != 0)
+                    return comparsion;
             }
+            int comparsion = Integer.compare(o1.length, o2.length);
+            if (comparsion != 0) return comparsion;
+            return o1[0].compareTo(o2[0]);
         });
         String[] result = new String[logs.length];
         int idx = 0;
-        for (String[] letterLog : letterList) {
-            StringBuilder sb = new StringBuilder();
-            for (String log : letterLog) {
-                sb.append(log);
-                sb.append(" ");
-            }
-            sb.deleteCharAt(sb.length()-1);
-            result[idx++] = sb.toString();
-        }
+        for (String[] letterLog : letterList)
+            result[idx++] = String.join(" ", letterLog);
         for (String digitLog : digitList)
             result[idx++] = digitLog;
         return result;
